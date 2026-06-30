f98836f8863060711035a716581bb79d7e65314b
ryohei-db <ryohei10251@outlook.jp>
2026-07-01 02:23:43 +0900

    pubfun_20260630_0223

--- pubfun/git/gsave.sh
+++ pubfun/git/gsave.sh
@@ -2,6 +2,19 @@
 
 #!/bin/bash
 
-local git_title="$changed_dirs"_"$today"_"$now_time"
+#変更されたフォルダ名を取得
 
+local changed_dirs=$(git diff --name-only | cut -d / -f 1 | sort -u )
+
+#変更日を取得
+
+local today=$(date +%F)
+
+#変更時間を取得
+
+local now_time=$(date +%H%M)
+
+#既存のファイル変更時のタイトル
+
+local git_title="$changed_dirs"_"$today"_"$now_time" 
 
