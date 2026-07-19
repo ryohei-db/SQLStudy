55efe64445a36eb65b44a17024db0927936ca689
ryohei-db <ryohei10251@outlook.jp>
2026-07-19 14:39:58 +0900

    pubfun__2026-07-19_1439

--- pubfun/bk_db.sh
+++ pubfun/bk_db.sh
@@ -3,15 +3,20 @@
 # bk_db() DBのバックアップの中身
 # 初期実装では else を用いて正常処理をネストしていたが、可読性向上のため Early Exit を採用し、異常系のみを if で処理する構成へリファクタリングした。
 
+# スクリプトの基準ディレクトリを取得
+
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+# データベースとバックアップ先のパス
 
 DB=""${BASE_DIR}"/db/study.db"
 
 BACKUP_DIR=""${BASE_DIR}"/db_backup"
 
 
+# データベースの存在確認
 
-if [ ! "${DB}" -f ]; then
+if [ ! -f "${DB}" ]; then
 
     echo ""${DB}"が存在しません"
 
@@ -21,8 +26,9 @@ fi
 
 echo ""${DB}"が存在します"
 
+# バックアップディレクトリがなければ作成
 
-if[ ! "${BACKUP_DIR}" -d ]; then
+if [ ! -d "${BACKUP_DIR}"  ]; then
 
     echo ""${BACKUP_DIR}"が存在しません"
 
@@ -30,11 +36,17 @@ if[ ! "${BACKUP_DIR}" -d ]; then
 
 fi
 
-BACKUP_NAME="study_"$(date '+Y%m%d%H%M%S')".db"
+# バックアップファイル名を作成
+
+BACKUP_NAME="study_"$(date '+%Y%m%d%H%M%S')".db"
+
+# データベースをバックアップ
 
 cp "${DB}" "${BACKUP_DIR}"/"${BACKUP_NAME}"
 
-if [ $? -eq 1 ]; then
+# バックアップ成功確認
+
+if [ $? -ne 0 ]; then
 
 echo "バックアップファイルの作成に失敗しました"
 
