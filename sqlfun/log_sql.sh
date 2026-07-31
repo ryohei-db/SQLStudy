238b1c19590249bc2184f8ed4c018f460262cf39
ryohei-db <ryohei10251@outlook.jp>
2026-08-01 01:31:20 +0900

    sqlfun__2026-08-01_01:31

--- sqlfun/log_sql.sh
+++ sqlfun/log_sql.sh
@@ -5,3 +5,6 @@ BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 # shellcheck source=/dev/null
 source "${BASE_DIR}/config/settings.conf"
 
+# shellcheck disable=SC2034
+start_datetime="$(date '+%Y-%m-%d %H:%M:%S')"
+
