65aa4ef2b4e0e520186403379e77bebf9d37f410
ryohei-db <ryohei10251@outlook.jp>
2026-08-01 01:55:51 +0900

    sqlfun__2026-08-01_01:55

--- sqlfun/log_sql.sh
+++ sqlfun/log_sql.sh
@@ -8,3 +8,12 @@ source "${BASE_DIR}/config/settings.conf"
 # shellcheck disable=SC2034
 start_datetime="$(date '+%Y-%m-%d %H:%M:%S')"
 
+
+if [ -t 0 ]; then
+    # 標準入力なし → SQLファイルを使う
+    sql_file="$1"
+    sqlite3 "${DB}" < "${sql_file}"
+else
+    # 標準入力あり → パイプから受け取る
+    sqlite3 "${DB}"
+fi
